81f4b2228bceb6dd96a9742b683d6ce2e175daeb
Rafael Vieira <rafaelleveske@gmail.com>
2020-08-17 19:51:12 -0300

    Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Header from './components/Header';
+
 import './styles/global.css';
 
 function App() {
   return (
     <div className="container">
+      <Header />
       <h1>Hello Rafa</h1>
     </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import logo from '../../assets/images/logo.svg';
+
+import './styles.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="side-content">
+        <img src={logo} alt="Logo" />
+        <strong>React Blog</strong>
+      </div>
+      <div className="search">
+        <input type="text" placeholder="Pesquisar artigo..." />
+        <button type="button">Pesquisar</button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
